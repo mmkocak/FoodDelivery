baf4ff541ee52e56cbd68089567a3bc0f116a6e6
muhammed <mmkocak6565@gmail.com>
2025-01-09 18:24:20 +0300

    Header main structure created, hamburger background and cross button added

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fooddelivery.ui.screen.ProductDetailsScreen
 import com.example.fooddelivery.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
-    }
+fun MainScreen(modifier: Modifier = Modifier) {
+ ProductDetailsScreen()
 }
--- app/src/main/java/com/example/fooddelivery/ui/screen/components/ProductPreviewSection.kt
+++ app/src/main/java/com/example/fooddelivery/ui/screen/components/ProductPreviewSection.kt
@@ -1,17 +1,33 @@
 package com.example.fooddelivery.ui.screen.components
 
 import android.app.ActionBar
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.fooddelivery.R
 import com.example.fooddelivery.ui.theme.AppColors
 import com.example.fooddelivery.ui.theme.AppTheme
 
@@ -19,8 +35,59 @@ import com.example.fooddelivery.ui.theme.AppTheme
 fun ProductPreviewSection(
     modifier: Modifier = Modifier
 ) {
-
+Box (modifier = modifier.height(IntrinsicSize.Max)){
+    ProductBackground(
+        modifier = modifier.padding(bottom = 24.dp)
+    )
+    Content(
+        modifier = modifier
+            .statusBarsPadding()
+            .padding(top = 24.dp)
+        )
+    }
 }
+
+@Composable
+private fun ProductBackground(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+        .fillMaxSize()
+            .background(color = AppTheme.Colors.secondarySurface,
+                shape = RoundedCornerShape(
+                    bottomStart = 32.dp,
+                    bottomEnd = 32.dp
+                )
+            )
+    )
+}
+@Composable
+private fun Content(modifier: Modifier = Modifier) {
+    ConstraintLayout(modifier = modifier.fillMaxWidth()) {
+        val (actionBar, highLights, productImage) = createRefs()
+        ActionBar(
+            headline = "Mr. Cheezy",
+            modifier = modifier
+                .padding(horizontal = 18.dp)
+                .constrainAs(actionBar) {
+                    top.linkTo(parent.top)
+                }
+            )
+        Image(
+            painter = painterResource(id = R.drawable.img_burger,),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .height(256.dp)
+                .constrainAs(productImage){
+                    end.linkTo(parent.end)
+                    top.linkTo(anchor = actionBar.bottom, margin = 20.dp)
+                }
+        )
+    }
+}
+
 @Composable
 private fun ActionBar(
     modifier: Modifier = Modifier,
@@ -35,5 +102,26 @@ private fun ActionBar(
         text = headline,
         style = AppTheme.Typography.headLine, color = AppTheme.Colors.onSecondarySurface,
         )
+        CloseButton()
+    }
+}
+@Composable
+private fun CloseButton(modifier: Modifier = Modifier){
+    Surface(
+        modifier = modifier.size(44.dp),
+        shape = RoundedCornerShape(16.dp),
+        color = AppTheme.Colors.actionSurface,
+        contentColor = AppTheme.Colors.secondarySurface
+    ) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_close),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp),
+            )
+        }
     }
 }

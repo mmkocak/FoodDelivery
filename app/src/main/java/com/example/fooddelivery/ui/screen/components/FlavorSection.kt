a7f056c68feee3e9badb12b6856cfbbdc0c7f99a
muhammed <mmkocak6565@gmail.com>
2025-01-14 20:29:13 +0300

    flavor section page created

---
+++ app/src/main/java/com/example/fooddelivery/ui/screen/components/FlavorSection.kt
@@ -0,0 +1,14 @@
+package com.example.fooddelivery.ui.screen.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FlavorSection(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+
+    }
+}

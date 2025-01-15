e394af418b1c7417b09377215aee04d648b60d53
muhammed <mmkocak6565@gmail.com>
2025-01-15 21:04:25 +0300

    flavor section added

--- app/src/main/java/com/example/fooddelivery/ui/screen/ProductDetailsScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/screen/ProductDetailsScreen.kt
@@ -1,11 +1,16 @@
 package com.example.fooddelivery.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.fooddelivery.data.ProductPreviewState
+import com.example.fooddelivery.ui.screen.components.FlavorSection
 import com.example.fooddelivery.ui.screen.components.ProductPreviewSection
 
 @Composable
@@ -19,6 +24,12 @@ fun ProductDetailsScreen(
     ) {
         ProductPreviewSection(
             state = productPreviewState
+        )
+        Spacer(
+            modifier = modifier.height(16.dp),
+            )
+        FlavorSection(
+            modifier = Modifier.padding(18.dp)
         )
     }
 }
--- app/src/main/java/com/example/fooddelivery/ui/screen/components/FlavorSection.kt
+++ app/src/main/java/com/example/fooddelivery/ui/screen/components/FlavorSection.kt
@@ -1,14 +1,44 @@
 package com.example.fooddelivery.ui.screen.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.ui.theme.AppTheme
 
 @Composable
 fun FlavorSection(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
+    SectionHeader(
+        title = "Add More Flavor",
+        emotion = "\uD83E\uDD29"
+    )
+    }
+}
 
+@Composable
+private fun SectionHeader(
+    modifier: Modifier = Modifier,
+    title: String,
+    emotion: String
+) {
+    Row (
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ){
+        Text(
+            text = title,
+            style = AppTheme.Typography.titleLarge,
+            color = AppTheme.Colors.onBackground
+        )
+        Text(
+            text = emotion,
+            style = AppTheme.Typography.titleLarge,
+        )
     }
 }
